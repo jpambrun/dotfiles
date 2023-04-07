2def387ea9e5a793e1447e65e21de70a4fed80b3
JF Pambrun <jean-francois.pambrun@changehealthcare.com>
2023-04-07 13:49:53 -0400

    wip

--- bin/install.sh
+++ bin/install.sh
@@ -19,6 +19,7 @@ curl https://i.ip.dev.health/casey/just | bash
 curl https://i.ip.dev.health/twpayne/chezmoi | bash
 curl https://i.ip.dev.health/mutagen-io/mutagen-compose | bash
 curl https://i.ip.dev.health/zellij-org/zellij | bash
+curl https://i.ip.dev.health/starship/starship | bash
 
 
 curl -L https://github.com/helix-editor/helix/releases/download/22.12/helix-22.12-x86_64-linux.tar.xz | tar xJf - --strip-components=1 -C /tmp
