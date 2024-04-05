5be3c6faf3e6152b85bdf7af6b0cc008d0b8379f
JF Pambrun <jfpambrun@vidalung.ai>
2024-04-05 16:16:02 -0400

    update

---
+++ bin/executable_dcm2json.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env -S deno run
+import dicomParser from "npm:dicom-parser";
+import { standardDataElements } from 'npm:dicom-data-dictionary'
+
+
+const stdin = await Deno.readAll(Deno.stdin)
+const dataSet = dicomParser.parseDicom(stdin, {
+    untilTag: "x7fe00010",
+})
+
+var options = {
+    omitPrivateAttibutes: true,
+    maxElementLength: 128
+};
+
+var instance = dicomParser.explicitDataSetToJS(dataSet, options);
+
+function renameKeys(source, dest) {
+    dest = dest || {}
+    for (let key in source) {
+        if (key[0] !== 'x' || source[key].dataOffset !== undefined) continue
+        const tag = key.slice(1, 9).toUpperCase()
+        let newKey = standardDataElements[tag] ? standardDataElements[tag].name : key
+        if (source[key] instanceof Array) {
+            dest[newKey] = source[key].map(renameKeys)
+        } else if (source[key] instanceof Object) {
+            dest[newKey] = renameKeys(source[key])
+        } else {
+            dest[newKey] = source[key]
+        }
+    }
+    return dest
+
+}
+
+const renamedInstance = renameKeys(instance);
+
+console.log(JSON.stringify(renamedInstance, null, 2))
+
+// fd -e dcm -x sh -c 'cat {} | deno run dcm2json.js {} > {.}.json'
+// duckdb :memory: 'select PatientName, InstanceNumber from read_json("*.json", maximum_depth=2, union_by_name=true)'
---
+++ bin/executable_extractDicomFromApk.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S deno run --allow-net --allow-env --allow-write
+import { S3Client } from "https://deno.land/x/s3_lite_client@0.7.0/mod.ts";
+import { read } from "https://deno.land/x/streaming_zip/read.ts";
+import { PartialReader } from "https://deno.land/x/stream_slicing/partial_reader.ts";
+import { Buffer } from "https://deno.land/std@0.195.0/streams/buffer.ts";
+import { parseArgs } from "https://deno.land/std@0.221.0/cli/parse_args.ts";
+
+const parsedArgs = parseArgs(Deno.args);
+if (!parsedArgs.bucket) throw new Exception('--bucket is required')
+if (!parsedArgs?._?.[0]) throw new Exception('path to apk is required')
+if (!parsedArgs._[0].endsWith('.apk')) throw new Exception('path needs to ned with .apk')
+const filename = parsedArgs._[0].split('/').pop().replace(/.apk$/, '')
+
+const s3client = new S3Client({
+    endPoint: "s3.us-east-2.amazonaws.com",
+    port: 443,
+    useSSL: true,
+    region: "us-east-2",
+    bucket: parsedArgs.bucket,
+    accessKey: Deno.env.get("AWS_ACCESS_KEY_ID"),
+    secretKey: Deno.env.get("AWS_SECRET_ACCESS_KEY"),
+    sessionToken: Deno.env.get("AWS_SESSION_TOKEN"),
+    pathStyle: false,
+});
+
+const resp = await s3client.getObject(parsedArgs._[0]);
+const partialReader = PartialReader.fromStream(resp.body);
+
+
+await Deno.mkdir(`${filename}/dicom`, { recursive: true })
+
+for await (const entry of read(partialReader)) {
+    if (entry.type === 'directory') continue;
+    const buffer = new Buffer();
+    await entry.body.stream().pipeTo(buffer.writable);
+    if (!entry.name.endsWith('.dcm')) continue
+    await Deno.writeFile(`./${filename}/${entry.name}`, buffer.bytes());
+    console.log(`Extracted ${entry.name}`)
+}
+
+
+// extractDicomFromApk.js --bucket vida-biobank-landing-bucket-sandbox-743078781022-us-east-2  apk_historical/academic-VIDA/c412df63728b65fca75035b10090ace1/1.3.12.2.1107.5.1.4.60078.30000011110919503346800003276/1.3.12.2.1107.5.1.4.60078.30000011110919503346800000718/JH113680_H-14551_20111109_132145_20200129_174224_export.apk
